a6a150c1d922bf0fbd55587568ac37d63fc6c977
paulsoerensen <paul.soerensen@gmail.com>
2024-06-09 22:11:11 +0200

    offline

--- MSCaddie.Client/obj/Debug/net8.0/MSCaddie.Client.AssemblyInfo.cs
+++ MSCaddie.Client/obj/Debug/net8.0/MSCaddie.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSCaddie.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96f62a7bb8329995ddbfc96ccd9d0bbe321b808")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSCaddie.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSCaddie.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
